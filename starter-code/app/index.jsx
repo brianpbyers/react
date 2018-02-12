e6faf0bf541e36038cb9c8996593652b23f4358a
mtclark0518 <mtclark0518@gmail.com>
2018-02-11 19:27:07 -0700

    removes practice notes

--- starter-code/app/index.jsx
+++ starter-code/app/index.jsx
@@ -2,10 +2,6 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MoodTracker from './components/hello-world'
 
 
-ReactDOM.render(
-  <MoodTracker name="WDI" mood="hyped" />,
-   document.getElementById('hello-world-component')
-)
+ReactDOM.render()
