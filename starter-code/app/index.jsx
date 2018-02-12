27b35c9c2d091be4882f0168bd71bdc25386a980
mtclark0518 <mtclark0518@gmail.com>
2018-02-11 19:36:14 -0700

    removes practice run

--- starter-code/app/index.jsx
+++ starter-code/app/index.jsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MoodTracker from './components/hello-world';
 
 
-ReactDOM.render(<MoodTracker name='fart' mood='happy' />, document.getElementById('hello-world-component'))
+ReactDOM.render()
